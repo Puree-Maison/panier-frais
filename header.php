48eb9e85e32957d398f26460d9365020a1203052
Paul Hunault <paul@pureemaison.com>
2015-03-15 13:17:06 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,61 @@
+  <!DOCTYPE html>
+  <html lang="fr">
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang=""fr" xml:lang="fr""> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang=""fr" xml:lang="fr""> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang=""fr" xml:lang="fr""> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie9 oldie" lang=""fr" xml:lang="fr""> <![endif]-->
+<!--[if gt IE 8]> <html class="no-js newie" lang='"fr" xml:lang="fr"'> <![endif]-->
+<head>
+  <meta charset="utf-8">
+  <title>Le Panier Frais</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/jquery.rateyo.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body id='top'>
+
+    <div class="container">            
+        <div class="row">
+            <div class="col-sm-3 col-md-5 col-xs-3 nopadding test">
+            <a href="/panier-frais/index.php">Logo</a>
+            </div>
+            <div class="col-sm-9 col-md-7 col-xs-9 nopadding test">
+                <div class="col-sm-10 hidden-xs">
+                  <form class="search-form" name="search-form" method="post">
+                        <div class="input-group col-sm-12 search-form">
+                            <input type="text" class="form-control" placeholder="Chercher un article" name="search">
+                            <div class="input-group-btn">
+                                <button type="button" class="btn btn-success"><span class="glyphicon glyphicon-search"></span></button>
+                            </div>
+                        </div>
+                  </form>
+                </div>
+                <div class="col-sm-2 col-xs-12">
+                  <button type="button" class="tcon tcon-menu--xbutterfly" aria-label="toggle menu">
+                    <span class="tcon-menu__lines" aria-hidden="true"></span>
+                    <span class="tcon-visuallyhidden">menu</span>
+                  </button>
+                </div>
+            </div> 
+                <div class="col-sm-8 nopaddingright test">
+                    
+                </div>
+                <div class="col-sm-2 hidden-xs nopadding test">
+                <a href="login.php">Mon espace</a>
+                </div>
+                <div class="col-sm-2 hidden-xs nopadding test">
+                <a href="panier.php">Mon panier</a>
+                </div>
+            </div>
+        </div>
+    </div>
