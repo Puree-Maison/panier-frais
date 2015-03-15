372bde3ef2e7720f3144912ae30d9caf25602f5f
Paul Hunault <paul@pureemaison.com>
2015-03-15 13:17:24 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,32 @@
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!-- Custom script -->
+    <script src="js/bouton-menu.js"></script>
+
+    <script src="js/jquery.rateyo.js"></script>
+
+</body>
+
+</html>
